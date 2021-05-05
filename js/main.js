193c10550df7feaf717a47b7d1b9b6a3cce47c46
Galina Bondar <galina.rizhniak@gmai.com>
2021-05-05 16:44:05 +0300

    Add carousel

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+ 
+    $("#owl-example").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1]
+    });
+ 
+});
+
+$(document).ready(function() {
+ 
+    $("#owl-example2").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,1],
+        itemsDesktopSmall: [979, 1]
+      
+    });
+ 
+});
