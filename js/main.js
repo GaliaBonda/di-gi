da413fc1b58aa4550cf67fbc97f771970ecffc2c
Galina Bondar <galina.rizhniak@gmai.com>
2021-05-06 10:45:28 +0300

    Add itemsTablet for carousel

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
  
     $("#intro-carousel").owlCarousel({
-        autoPlay: 4000, //Set AutoPlay to 3 seconds
+        autoPlay: 4000, //Set AutoPlay to 4 seconds
  
       items : 1,
       itemsDesktop : [1199,1],
-      itemsDesktopSmall : [979,1]
+      itemsDesktopSmall: [979, 1],
+      itemsTablet: [768,1]
     });
  
 });
@@ -13,11 +14,12 @@ $(document).ready(function() {
 $(document).ready(function() {
  
     $("#testimonials-carousel").owlCarousel({
-        autoPlay: 5000, //Set AutoPlay to 3 seconds
+        autoPlay: 5000, //Set AutoPlay to 5 seconds
  
       items : 1,
       itemsDesktop : [1199,1],
-        itemsDesktopSmall: [979, 1]
+      itemsDesktopSmall: [979, 1],
+      itemsTablet: [768,1]
       
     });
  
